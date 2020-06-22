a42dbe4ae8486a0546fc8ee5432c4caccd471438
Kaleigh Spurio <kaleighastell@gmail.com>
2020-06-22 13:15:33 -0400

    set up the server

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,7 +1,13 @@
-// require burger.js
 const express = require('express');
 // create router and export the router
 const router = express().route();
+
+const burger = require("../models/burger");
+
+router.get("/", (req, res) =>{});
+
+router.post("/api/burgers", (req,res) => {});
+
 
 
 module.export = router;
--- server.js
+++ server.js
@@ -1,1 +1,23 @@
 const express = require("express");
+const app = express();
+const exphbs = require("express-handlebars");
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+
+// middleware that is needed to use POST data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/burgers_controller");
+
+app.use(routes);
+
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
